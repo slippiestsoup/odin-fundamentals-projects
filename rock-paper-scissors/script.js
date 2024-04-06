dc23f1ad1780526ece7c7c6faf2ccebfb9ee8e4b
Jessica <155894669+slippiestsoup@users.noreply.github.com>
2024-04-06 11:56:17 -0400

    Add playerchoice and playround functions. playround function does not take playerchoice or computer choice into account. playround function only prints first if statement.

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -3,3 +3,33 @@ function getComputerChoice() {
     const rand = list[Math.floor(Math.random() * list.length)];
     return rand
 };
+
+function getPlayerChoice() {
+    const userInput = window.prompt('Enter a choice (rock, paper, scissors):')
+};
+
+let compResult = getComputerChoice();
+
+let playerResult = getPlayerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerResult==='paper' && compResult==='scissors') {
+       return 'You lose! Scissors beats paper!';
+    } else if (playerResult==='scissors' && compResult==='rock') {
+        return 'You lose! Rock beats scissors!';
+    } else if (playerResult==='rock' && compResult==='paper') {
+        return 'You lose! Paper beats rock!';
+    } else if (playerResult==='paper' && compResult==='rock') {
+        return 'You win! Paper beats rock!';
+    } else if (playerResult==='rock' && compResult==='scissors') {
+        return 'You win! Rock beats scissors!';
+    } else if (playerResult==='scissors' && compResult==='paper') {
+        return 'You win! Scissors beats paper!';
+    } else if (playerResult==='rock' && compResult==='rock') {
+        return 'Tie!';
+    } else if (playerResult==='paper' && compResult==='paper') {
+        return 'Tie!';
+    } else if (playerResult==='scissors' && compResult==='scissors') {
+        return 'Tie!';
+    }
+}
