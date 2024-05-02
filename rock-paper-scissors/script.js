9b928756b8ba3613bd5333bb45d480b485b3a99a
Jessica <155894669+slippiestsoup@users.noreply.github.com>
2024-05-01 20:12:29 -0400

    add score and choice text and updates

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,3 +1,10 @@
+const choices = document.querySelector("#choices");
+const scores = document.querySelector("#scores");
+const choicePara = document.querySelector("#choicePara");
+const scorePara = document.querySelector("#scorePara");
+choices.appendChild(choicePara);
+scores.appendChild(scorePara);
+
 const list = ["rock", "paper", "scissors"];
 
 function getCompChoice() {
@@ -8,30 +15,31 @@ function getCompChoice() {
 let playerScore = 0;
 let compScore = 0;
 
+function updateChoice(playerResult, compResult) {
+  choicePara.textContent = ""
+  choicePara.textContent = `Player: ${playerResult}. Computer: ${compResult}.`;
+};
+
+function updateScore(playerScore, compScore) {
+  scorePara.textContent = ""
+  scorePara.textContent = `Player: ${playerScore}. Computer: ${compScore}.`;
+}
+
 function playRound(playerResult) {
   let compResult = getCompChoice();
-  console.log(`Player: ${playerResult}. Computer: ${compResult}.`);
   if (playerResult === compResult) {
     playerScore++;
     compScore++;
-    console.log(`Player: ${playerScore}. Computer: ${compScore}.`);
-    return;
   } else if (playerResult === "scissors" && compResult === "paper") {
     playerScore++;
-    console.log(`Player: ${playerScore}. Computer: ${compScore}.`);
-    return;
   } else if (playerResult === "paper" && compResult === "rock") {
     playerScore++;
-    console.log(`Player: ${playerScore}. Computer: ${compScore}.`);
-    return;
   } else if (playerResult === "rock" && compResult === "scissors") {
     playerScore++;
-    console.log(`Player: ${playerScore}. Computer: ${compScore}.`);
-    return;
   } else
     compScore++;
-    console.log(`Player: ${playerScore}. Computer: ${compScore}.`);
-    return;
+  updateChoice(playerResult, compResult);
+  updateScore(playerScore, compScore);
 }
 
 let rockGame = document.querySelector("#rock");
@@ -47,17 +55,6 @@ paperGame.addEventListener("click", () => {
 scissorsGame.addEventListener("click", () => {
   playRound("scissors");
 });
-
-/* run player choice as an alert window
-function getPlayerChoice() {
-  let userInput = window.prompt("Enter a choice (rock, paper, scissors):");
-  if (list.includes(userInput.toLowerCase())) {
-    return userInput;
-  } else {
-    console.log("Please enter a valid option.");
-    return getPlayerChoice();
-  }
-}*/
 
 /* Play 5 rounds in a row with score tracking.
 function playMatch() {
