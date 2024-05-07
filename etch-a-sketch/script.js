d33167132ba9b3466ff06f1a350670db173a4d97
Jessica <155894669+slippiestsoup@users.noreply.github.com>
2024-05-06 23:28:53 -0400

    change create grid function into rows and columns functions. Add hover styling.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,14 +1,25 @@
-const container = document.querySelector("#container");
-const baseGrid = {rows: 16, cols: 16};
-const totalGrid = baseGrid.rows * baseGrid.cols;
+const container = document.querySelector(".container");
+const cells = document.querySelector(".cells")
 
-function createGrid(tot) {
-    for (let i=0; i<tot; i++) {
-    let eleGrid = document.createElement('div');
-    container.appendChild(eleGrid);
-    eleGrid.classList.add('cells');
-    };
-};
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    const eleCol = document.createElement("div");
+    container.appendChild(eleCol);
+    eleCol.setAttribute(
+      "style",
+      "border: 0.25px solid black; display: flex; flex-direction: column; gap: 1px; align-content: stretch; flex: 1;"
+    );
 
-createGrid(totalGrid);
+    for (let j = 0; j < 16; j++) {
+      const eleRow = document.createElement("div");
+      eleCol.appendChild(eleRow);
+      eleRow.classList.add("cells");
+      eleRow.setAttribute(
+        "style",
+        "border: 0.25px solid black; display: flex; flex-direction: row; gap: 1px; align-content: stretch; flex: 1;"
+      );
+    }
+  }
+}
 
+createGrid();
